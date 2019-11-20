b5bab3b8b938635956eaa40dde12e59c2230dbf0
holgermanzke <manzkeholger@gmail.com>
2019-11-20 12:34:04 +0100

    add styled components with emotion and delate app.css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
-import "./App.css";
 import Game from "./components/Game";
+import styled from "@emotion/styled";
+
+const AppStyle = styled.div`
+  text-align: center;
+  background-color: #282c34;
+  min-height: 100vh;
+  padding: 20px;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
 
 function App() {
   return (
-    <div className="App">
+    <AppStyle>
       <h1>Tic Tac Toe</h1>
 
       <Game />
-    </div>
+    </AppStyle>
   );
 }
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,28 @@
 import React from "react";
 import Board from "./Board";
+import styled from "@emotion/styled";
+
+const GameContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const GameInfo = styled.div`
+  margin-left: 20px;
+`;
+
+const Ol = styled.ol`
+  padding-left: 30px;
+`;
 
 export default function Game() {
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board />
-      </div>
-      <div className="game-info">
+    <GameContainer>
+      <Board />
+      <GameInfo>
         <div>{/* status */}</div>
-        <ol>{/* TODO */}</ol>
-      </div>
-    </div>
+        <Ol>{/* TODO */}</Ol>
+      </GameInfo>
+    </GameContainer>
   );
 }
