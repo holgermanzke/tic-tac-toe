fd60e0a23f2b59d91ce6c7cb512843069b454c21
holgermanzke <56833949+holgermanzke@users.noreply.github.com>
2019-11-20 13:36:30 +0100

    Emotion (#2)
    * install emotion / styled components
    
    * add styled components with emotion and delate app.css
    
    * add styled components with emotion and delate app.css

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,11 +1,27 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Button = styled.button`
+  background: #fff;
+  border: 1px solid #999;
+  float: left;
+  font-size: 24px;
+  font-weight: bold;
+  line-height: 34px;
+  height: 34px;
+  margin-right: -1px;
+  margin-top: -1px;
+  padding: 0;
+  text-align: center;
+  width: 34px;
+  :focus {
+    outline: none;
+  }
+`;
 
 export default function Square({ value, onClick }) {
   // console.log("rendered square with value", value);
   // const [value, setValue] = useState(null);
-  return (
-    <button className="square" onClick={onClick}>
-      {value}
-    </button>
-  );
+
+  return <Button onClick={onClick}>{value}</Button>;
 }
